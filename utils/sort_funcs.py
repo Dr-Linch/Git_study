0084177abd5d02f4eb8d078ea6d1940248abc03d
Linch <dr.linch19@gmail.com>
2023-10-13 21:57:20 +0300

    Write litle function for json file

--- utils/sort_funcs.py
+++ utils/sort_funcs.py
@@ -1,3 +1,17 @@
-print('file_2')
+import os
+import json
 
-#just comment
+with open(os.path.join('data', 'posts.json'), 'rt', encoding='utf-8') as json_file:
+    data = json.load(json_file)
+
+users_info: dict = {'users': []}
+users_keys = ['id', 'title']
+
+for item in data:
+    short_info: dict = {}
+    for key in users_keys:
+        short_info[key] = item[key]
+
+    users_info['users'].append(short_info)
+
+print(users_info)
