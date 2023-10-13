aa1440c0815187a470e54db94f0810bad2b3c5f0
Linch <dr.linch19@gmail.com>
2023-10-13 22:27:44 +0300

    Made greetin dunc

--- 11.1/main.py
+++ 11.1/main.py
@@ -1,1 +1,9 @@
-print('file_1')
+user_name: str = input('Please enter your name!\n:')
+
+
+def hello_user(name: str):
+    start_answer: dict = {'yes': True, 'no': False}
+    print(f'Hi {name}, glad to see you!')
+    start_question: str = input('Do you want start the game?\n:')
+
+    return start_answer[start_question.lower()]
