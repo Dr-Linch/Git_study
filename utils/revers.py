d06b13d74cb5e8d3c40dc49d485c5d6ea773dd24
Linch <dr.linch19@gmail.com>
2023-10-16 21:18:31 +0300

    Normal code

--- utils/revers.py
+++ utils/revers.py
@@ -2,13 +2,7 @@ some_text = "Hi guis, my name is Linch"
 
 
 def text_revers(text: str):
-    text_list = []
-
-    for letter in text:
-        text_list.append(letter)
-
-    text_list.reverse()
-    return ''.join(text_list)
+    return some_text[::-1]
 
 
 print(text_revers(some_text))
